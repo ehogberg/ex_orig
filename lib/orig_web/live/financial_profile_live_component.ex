f5738c3d31477156da423544fed30a90b2b04725
Eric Hogberg <misc@erichogberg.com>
2022-04-23 00:39:25 -0500

    Turn off auto-disable on fin profile form.

--- lib/orig_web/live/financial_profile_live_component.ex
+++ lib/orig_web/live/financial_profile_live_component.ex
@@ -72,7 +72,7 @@ defmodule OrigWeb.FinancialProfileLiveComponent do
         <.select f={f} field="pay_period" opts={PayPeriod.__enums__}/>
         <.text_entry f={f} field="primary_routing_number" />
         <.text_entry f={f} field="primary_account_number" />
-        <.form_submit disabled={!(@changeset.valid?)}>Save Financial Profile Information</.form_submit>
+        <.form_submit>Save Financial Profile Information</.form_submit>
       </.form>
     </section>
     """
